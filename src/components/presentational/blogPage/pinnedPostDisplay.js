87a41b0e7524711560fab3b72b4765c5d296b61d
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-10-21 15:13:03 -0400

    Post Displays no longer show html tag formatting

--- src/components/presentational/blogPage/pinnedPostDisplay.js
+++ src/components/presentational/blogPage/pinnedPostDisplay.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import propTypes from 'prop-types';
 
-const pinnedPostDisplay = ({ post }) => {
+const PinnedPostDisplay = ({ post }) => {
   const { title } = post;
   const shortTitle = title.length > 30 ? `${title.substring(0, 30)}...` : title;
+  const bodyElem = useRef(null);
+
+  useEffect(() => {
+    const body = bodyElem.current;
+    if (body) {
+      // temporary div is created to remove html tag formatting
+      const tempBody = document.createElement('DIV');
+      tempBody.innerHTML = post.body.substring(0, 70);
+      body.innerHTML = `${tempBody.textContent}...`;
+    }
+  });
 
   return (
     <div className="pinned-post">
       <h4 className="pinned-post-title">{shortTitle}</h4>
       <h5 className="pinned-post-author">{post.author}</h5>
-      <div className="pinned-post-body">{`${post.body.substring(0, 70)}...`}</div>
+      <div ref={bodyElem} className="pinned-post-body" />
       <div className="pinned-post-date">{post.date}</div>
     </div>
   );
 };
 
-pinnedPostDisplay.propTypes = {
+PinnedPostDisplay.propTypes = {
   post: propTypes.instanceOf(Object).isRequired,
 };
 
-export default pinnedPostDisplay;
+export default PinnedPostDisplay;
--- src/components/presentational/blogPage/postDisplay.js
+++ src/components/presentational/blogPage/postDisplay.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import propTypes from 'prop-types';
 
 const PostDisplay = ({ post }) => {
   const { title } = post;
   const shortTitle = title.length > 30 ? `${title.substring(0, 30)}...` : title;
+  const bodyElem = useRef(null);
+
+  useEffect(() => {
+    const body = bodyElem.current;
+    if (body) {
+      // temporary div is created to remove html tag formatting
+      const tempBody = document.createElement('DIV');
+      tempBody.innerHTML = post.body.substring(0, 20);
+      body.innerHTML = `${tempBody.textContent}...`;
+    }
+  });
 
   return (
     <div className="post">
       <h4 className="post-title">{shortTitle}</h4>
       <div className="post-author">{post.author}</div>
-      <div className="post-body">{`${post.body.substring(0, 20)}...`}</div>
+      <div ref={bodyElem} className="post-body" />
       <div className="post-date">{post.date}</div>
     </div>
   );
